57c66ed4e7d437564ab195b72a5a365954c9e374
StevenYuan666 <ye.yuan3@mail.mcgill.ca>
2024-04-02 09:40:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -164,7 +164,7 @@ def parse_responses_phi2(outputs):
 
 def query_mistral7b(payload):
     API_URL = "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.2"
-    headers = {"Authorization": "Bearer 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"}
+    headers = {"Authorization": ""}
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
 
