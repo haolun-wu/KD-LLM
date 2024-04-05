93ad2d3f8a2c20efeb912117c47d02741b12dc09
Chengming Hu <55328995+Chengming0501@users.noreply.github.com>
2024-04-05 16:14:53 -0400

    Add phi-2 model

--- configs.py
+++ configs.py
@@ -7,7 +7,8 @@
 # DEFAULT_NUM_BEAMS = 5
 
 # DEFAULT_MODEL_NAME = "openai-community/gpt2-medium"
-DEFAULT_MODEL_NAME = "openlm-research/open_llama_3b_v2"
+# DEFAULT_MODEL_NAME = "openlm-research/open_llama_3b_v2"
+DEFAULT_MODEL_NAME = "microsoft/phi-2"
 DEFAULT_DATASET_PATH = "data/nli/validation.json"
 DEFAULT_MAX_LENGTH = 50
 DEFAULT_NUM_BEAMS = 1
